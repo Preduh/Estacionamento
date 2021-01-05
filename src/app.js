a1c3d8284ea526b054d564bdf8176487b65ce5ba
Preduh <pedro.lucas5860@gmail.com>
2021-01-05 13:20:45 -0300

    Pagamento

--- src/app.js
+++ src/app.js
@@ -13,43 +13,6 @@ app.set('views', path.join(__dirname + 'views'))
 app.use('/public', express.static('public'))
 
 // Rotas
-app.get('/estacionamento', (req, res) => {
-    Veiculo.find().then(veiculos => {
-        let modelos = []
-        let placas = []
-        let horarios = []
-        let ids = []
-        for (let i in veiculos) {
-            modelos.push(veiculos[i].modelo)
-            placas.push(veiculos[i].placa)
-            horarios.push(veiculos[i].horario)
-            ids.push(veiculos[i]._id)
-        }
-        res.render('../views/estacionamento', { ids: ids, modelos: modelos, placas: placas, horarios: horarios })
-    }).catch(err => {
-        console.log(err)
-    })
-})
-
-app.get('/estacionamento/:search', (req, res) => {
-    const search = req.params.search.toUpperCase()
-    Veiculo.find({ placa: new RegExp(search, 'i') }).then(veiculos => {
-        let modelos = []
-        let placas = []
-        let horarios = []
-        let ids = []
-        for (let i in veiculos) {
-            modelos.push(veiculos[i].modelo)
-            placas.push(veiculos[i].placa)
-            horarios.push(veiculos[i].horario)
-            ids.push(veiculos[i]._id)
-        }
-        res.render('../views/estacionamento', { ids: ids, modelos: modelos, placas: placas, horarios: horarios })
-    }).catch(err => {
-        console.log(err)
-    })
-})
-
 require('./controllers/authController')(app)
 
 // Definindo conexão
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,24 @@ router.get('/home', async (req, res) => {
     res.render('../views/home')
 })
 
+router.get('/estacionamento', (req, res) => {
+    Veiculo.find().then(veiculos => {
+        let modelos = []
+        let placas = []
+        let horarios = []
+        let ids = []
+        for (let i in veiculos) {
+            modelos.push(veiculos[i].modelo)
+            placas.push(veiculos[i].placa)
+            horarios.push(veiculos[i].horario)
+            ids.push(veiculos[i]._id)
+        }
+        res.render('../views/estacionamento', { ids: ids, modelos: modelos, placas: placas, horarios: horarios })
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
 router.post('/estacionamento', async (req, res) => {
     const { placa } = req.body
 
@@ -34,19 +52,50 @@ router.post('/estacionamento', async (req, res) => {
     }
 })
 
+router.get('/estacionamento/:search', (req, res) => {
+    const search = req.params.search.toUpperCase()
+    Veiculo.find({ placa: new RegExp(search, 'i') }).then(veiculos => {
+        let modelos = []
+        let placas = []
+        let horarios = []
+        let ids = []
+        for (let i in veiculos) {
+            modelos.push(veiculos[i].modelo)
+            placas.push(veiculos[i].placa)
+            horarios.push(veiculos[i].horario)
+            ids.push(veiculos[i]._id)
+        }
+        res.render('../views/estacionamento', { ids: ids, modelos: modelos, placas: placas, horarios: horarios })
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
 router.post('/search', async (req, res) => {
     const search = req.body.search
     res.redirect('/estacionamento/'+ search)
 })
 
 router.post('/finalizar/:id', async (req, res) => {
+    const idDelete = req.params.id
+    const veiculoAtual = []
+    try { 
+        await Veiculo.findById(idDelete).then(veiculo => {
+            veiculoAtual.push(veiculo.modelo, veiculo.placa)
+        })
+    } catch {
+
+    }
+    return res.render('../views/pagamento', { idDelete: idDelete, veiculoAtual: veiculoAtual })
+})
+
+router.post('/pagamento/:id', async (req, res) => {
     try {
         await Veiculo.findByIdAndDelete(req.params.id)
     } catch (err) {
         return res.send('Erro ao apagar')
     }
-
-    return res.redirect('/estacionamento')
+    res.redirect('/estacionamento')
 })
 
 module.exports = app => app.use(router)
